9cec7d55920742c4d11d5446a2cee17ad8c94f47
KJeff01 <kjeff1997@gmail.com>
2018-11-24 18:29:47 -0600

    Add initial research to 1-7 mission.

--- data/base/script/campaign/cam1-7.js
+++ data/base/script/campaign/cam1-7.js
@@ -3,17 +3,21 @@ include("script/campaign/libcampaign.js");
 include("script/campaign/templates.js");
 
 const SCAVS = 7; //Scav player number
-const NEW_PARADIGM_RESEARCH = [
-	"R-Wpn-MG-Damage04", "R-Wpn-MG-ROF01", "R-Defense-WallUpgrade03",
-	"R-Struc-Materials03", "R-Struc-Factory-Upgrade03", "R-Vehicle-Engine03",
-	"R-Vehicle-Metals03", "R-Cyborg-Metals03", "R-Wpn-Cannon-Accuracy01",
-	"R-Wpn-Cannon-Damage03", "R-Wpn-Flamer-Damage03", "R-Wpn-Flamer-ROF01",
-	"R-Wpn-Mortar-Damage03", "R-Wpn-Mortar-Acc01", "R-Wpn-Rocket-Accuracy01",
-	"R-Wpn-Rocket-Damage03", "R-Wpn-Rocket-ROF03", "R-Wpn-RocketSlow-Accuracy02",
-	"R-Wpn-RocketSlow-Damage03", "R-Struc-RprFac-Upgrade03",
+const NEW_PARADIGM_RES = [
+	"R-Wpn-MG1Mk1", "R-Vehicle-Body01", "R-Sys-Spade1Mk1", "R-Vehicle-Prop-Wheels",
+	"R-Sys-Engineering01", "R-Wpn-MG-Damage04", "R-Wpn-MG-ROF02", "R-Wpn-Cannon-Damage03",
+	"R-Wpn-Flamer-Damage03", "R-Wpn-Flamer-Range01", "R-Wpn-Flamer-ROF01",
+	"R-Defense-WallUpgrade03","R-Struc-Materials03", "R-Vehicle-Engine03",
+	"R-Struc-RprFac-Upgrade03", "R-Wpn-Rocket-Damage03", "R-Wpn-Rocket-ROF03",
+	"R-Vehicle-Metals03", "R-Wpn-Mortar-Damage03", "R-Wpn-Rocket-Accuracy02",
+	"R-Wpn-RocketSlow-Damage03", "R-Wpn-Mortar-ROF01", "R-Cyborg-Metals03",
+	"R-Wpn-Mortar-Acc01", "R-Wpn-RocketSlow-Accuracy02", "R-Wpn-Cannon-Accuracy01",
 ];
 const SCAVENGER_RES = [
-	"R-Wpn-MG-Damage03", "R-Wpn-Rocket-Damage02", "R-Wpn-Cannon-Damage02",
+	"R-Wpn-Flamer-Damage03", "R-Wpn-Flamer-Range01", "R-Wpn-Flamer-ROF01",
+	"R-Wpn-MG-Damage04", "R-Wpn-MG-ROF02", "R-Wpn-Rocket-Damage03", "R-Wpn-Cannon-Damage03",
+	"R-Wpn-Rocket-Accuracy02", "R-Wpn-Rocket-ROF02", "R-Vehicle-Metals03",
+	"R-Defense-WallUpgrade03", "R-Struc-Materials03", "R-Wpn-Cannon-Accuracy01",
 ];
 var artiGroup; //Droids that take the artifact
 var enemyHasArtifact; //Do they have the artifact
